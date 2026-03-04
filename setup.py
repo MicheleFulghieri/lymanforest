d0cdbff93a6efd772c4afbaa7d36b43a2dc4542a
Michele Fulghieri <m.fulghieri@campus.unimib.it>
2026-03-04 16:18:43 +0100

    Update to version 0.1.1 with simplified imports and fixed README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lymanforest',
-    version='0.1.0',
+    version='0.1.1',
     author='Michele',
     description='Pipeline to process the Lyman-alpha forest from TNG simulation and test the spectrum of a qso',
     packages=find_packages(),  
